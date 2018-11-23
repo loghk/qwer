3650be11c5c86faa4ae6097448013e0a5cfe3e73
sxz <1138538400@qq.com>
2018-11-24 00:16:35 +0800

    finash

---
+++ src/AWT/Work1.java
@@ -0,0 +1,79 @@
+package AWT;
+/*
+* 随机数生成器，图形界面
+* */
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
+
+public class Work1 {
+    public static void main(String[] args) {
+        Event event = new Event();
+        event.init();
+        event.run();
+    }
+}
+
+class Event {
+    Frame frame = new Frame("随机数");
+    Button stop = new Button("Stop");
+    Button begin = new Button("Begin");
+    TextArea text = new TextArea(1, 1);
+
+    public void init() {
+        frame.setSize(800, 800);
+        Panel panel = new Panel();
+        panel.add(stop);
+        panel.add(begin);
+        frame.setLayout(new BorderLayout(10, 100));
+        frame.add(text);
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.setVisible(true);
+    }
+    //管理容器运行的线程
+    public void run(){
+        TextThread textThread = new TextThread();
+        textThread.start();
+        stop.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textThread.suspend();
+            }
+        });
+
+        begin.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textThread.resume();
+            }
+        });
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    class TextThread extends Thread{
+        Random random = new Random();
+        @Override
+        public void run() {
+            while (true){
+                text.setFont(new Font("黑体", Font.BOLD, 500));
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                text.setText(String.valueOf(random.nextInt(50)));
+            }
+        }
+
+    }
+
+}
