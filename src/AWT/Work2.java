0d45064edce812677ce56bfc952557f7071a969c
sxz <1138538400@qq.com>
2018-11-24 17:01:56 +0800

    GUI计算器

--- src/AWT/Work2.java
+++ src/AWT/Work2.java
@@ -1,30 +1,232 @@
 package AWT;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Arrays;
 
 public class Work2 {
-    public static void main(String[] args)
-    {
-        new MyCalculator().init();
+    public static void main(String[] args) {
+        MyCalculator m = new MyCalculator();
+        m.init();
+        m.run();
     }
 }
 
-class MyCalculator{
+class MyCalculator {
+    double number;
+
     Frame frame = new Frame("计算器");
-    TextArea textArea = new TextArea(1,1);
+    TextArea textArea = new TextArea(1, 1);
     Panel panel = new Panel();
-    String[] name = {"AC","+/-","%","/","7","8","9","*","4","5","6","-","1","2","3","+","0",".","="};
-    public void init(){
+    String[] name = {"AC", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="};
+    Button[] buttons = new Button[name.length];
+
+    public void init() {
+        for (int i = 0; i < name.length; i++) {
+            buttons[i] = new Button(name[i]);
+        }
         textArea.setFont(new Font("黑体", Font.BOLD, 60));
         frame.setSize(500, 500);
-        frame.add(textArea,BorderLayout.NORTH);
-        panel.setLayout(new GridLayout(5,4));
-        for (int i = 0;i<name.length;i++){
-            panel.add(new Button(name[i]));
+        frame.add(textArea, BorderLayout.NORTH);
+        GridBagLayout gbl = new GridBagLayout();
+        panel.setLayout(gbl);
+        GridBagConstraints gbc = new GridBagConstraints();
+        for (int i = 0; i < name.length; i++) {
+            gbc.weighty = 1;
+            gbc.weightx = 1;
+            gbc.gridwidth = 1;
+            if (name[i].equals("0")) {
+                gbc.fill = GridBagConstraints.BOTH;
+                gbc.gridwidth = 2;
+                gbc.gridheight = 1;
+                gbl.setConstraints(buttons[i], gbc);
+                panel.add(buttons[i]);
+            } else {
+                if (name[i].equals("/") || name[i].equals("*") || name[i].equals("-") || name[i].equals("+")) {
+                    gbc.gridwidth = GridBagConstraints.REMAINDER;
+                }
+                gbc.fill = GridBagConstraints.BOTH;
+                gbl.setConstraints(buttons[i], gbc);
+                panel.add(buttons[i]);
+            }
         }
         frame.add(panel);
         frame.setVisible(true);
+    }
+
+    public void run() {
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        buttonListener();
+    }
+
+    public void buttonListener() {
+        // AC
+        buttons[0].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.setText("");
+                number = 0;
+            }
+        });
+        // +/-
+        buttons[1].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.setText("-" + textArea.getText());
+                number = 0 - number;
+            }
+        });
+        // %
+        buttons[2].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                double temp = Double.valueOf(textArea.getText());
+                textArea.setText(String.valueOf(temp / 100));
+            }
+        });
+        // /
+        buttons[3].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("/");
+            }
+        });
+        // 7
+        buttons[4].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("7");
+            }
+        });
+        // 8
+        buttons[5].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("8");
+            }
+        });
+        // 9
+        buttons[6].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("9");
+            }
+        });
+        // *
+        buttons[7].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("*");
+            }
+        });
+        // 4
+        buttons[8].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("4");
+            }
+        });
+        // 5
+        buttons[9].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("5");
+            }
+        });
+        // 6
+        buttons[10].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("6");
+            }
+        });
+        // -
+        buttons[11].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("-");
+            }
+        });
+        // 1
+        buttons[12].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("1");
+            }
+        });
+        // 2
+        buttons[13].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("2");
+            }
+        });
+        // 3
+        buttons[14].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("3");
+            }
+        });
+        // +
+        buttons[15].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("+");
+            }
+        });
+        // 0
+        buttons[16].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append("0");
+            }
+        });
+        // .
+        buttons[17].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textArea.append(".");
+            }
+        });
+        // =
+        buttons[18].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                mathematics(textArea.getText());
+            }
+        });
+
+
+    }
+
+    public void mathematics(String string) {
+        String[] str = string.split("[+*/\\-]");
+        System.out.println(Arrays.toString(str));
+        double num1 = Double.valueOf(str[0]);
+        double num2 = Double.valueOf(str[1]);
+        char[] temp = string.toCharArray();
+        char sign = '1';
+        for (char c : temp){
+            if (c=='+'||c=='-'||c=='*'||c=='/'){
+                sign = c;
+            }
+        }
+        double ans = 0.0;
+        switch (sign){
+            case '+': ans = num1+num2;break;
+            case '-': ans = num1-num2;break;
+            case '*': ans = num1*num2;break;
+            case '/': ans = num1/num2;break;
+        }
+        textArea.setText(String.valueOf(ans));
     }
 }
