cef5eb15ea07bbc80a704467882333d41968ef76
sxz <1138538400@qq.com>
2018-11-30 20:05:59 +0800

    聊天室带图形界面

--- src/Net/NewClient.java
+++ src/Net/NewClient.java
@@ -1,41 +1,54 @@
 package Net;
 
+import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class NewClient {
-    public static void main(String[] args){
-        String temp = new String();
+    public static void main(String[] args) {
+        StringBuffer temp = new StringBuffer();
+        TextArea get_message = new TextArea();
+        TextArea peoplelist = new TextArea();
+        Gui gui = new Gui(temp,get_message,peoplelist);
+        gui.init();
+        gui.run();
         Socket socket = null;
         try {
-            socket = new Socket("localhost",6666);
+            socket = new Socket("localhost", 6666);
         } catch (IOException e) {
             System.out.println("server is not exist");
             return;
         }
-        new Thread(new NewClientsup(socket)).start();
-        Scanner scanner = new Scanner(System.in);
+        new Thread(new NewClientsup(socket,get_message,peoplelist)).start();
         try (
                 OutputStream os = socket.getOutputStream();
                 PrintStream ps = new PrintStream(os);
-                ){
-            while (true){
-                   temp = scanner.next();
-                   ps.println(temp);
+        ) {
+            while (true) {
+                if (!temp.toString().equals("")){
+                    ps.println(temp);
+                    temp = temp.replace(0, temp.length(), "");
+                }
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
 
-class NewClientsup implements Runnable{
+class NewClientsup implements Runnable {
     private Socket socket;
     private BufferedReader br;
     private String temp;
-    public NewClientsup(Socket socket){
+    TextArea get_message;
+    TextArea peoplelist;
+
+    public NewClientsup(Socket socket,TextArea textArea,TextArea peoplelist) {
         this.socket = socket;
+        get_message = textArea;
+        this.peoplelist = peoplelist;
         try {
             br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
         } catch (IOException e) {
@@ -46,13 +59,112 @@ class NewClientsup implements Runnable{
     @Override
     public void run() {
         try {
-            while ((temp = br.readLine())!=null){
-                System.out.println(temp);
+            while ((temp = br.readLine()) != null) {
+                if (IsInform(temp)){
+                    temp = temp.substring(3, temp.length()-3);
+                    String[] strings = temp.split(" ");
+                    StringBuilder sb = new StringBuilder();
+                    for (String s : strings){
+                        sb.append(s+"\n");
+                    }
+                    peoplelist.setText(sb.toString());
+                }
+                else {
+                    get_message.append(temp+"\r\n");
+                }
             }
             br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public boolean IsInform(String string){
+        if (string.matches("@#[\\s\\S]*#@")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+class Gui implements Runnable {
+    StringBuffer stringBuffer;
+    Frame frame = new Frame("聊天室");
+    TextArea get_message = new TextArea();
+    TextArea send_message = new TextArea();
+    TextArea people_list = new TextArea();
+    Button button = new Button("SEND");
+    Panel panel = new Panel();
+
+    public Gui(StringBuffer string,TextArea textArea,TextArea people_list) {
+        this.stringBuffer = string;
+        get_message = textArea;
+        this.people_list = people_list;
+    }
+
+    public void init() {
+        //frame.setSize(800, 800);
+        frame.add(people_list, BorderLayout.EAST);
+        GridBagLayout gbl = new GridBagLayout();
+        panel.setLayout(gbl);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(20, 10, 50, 10);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.gridheight = 400;
+        gbl.setConstraints(get_message, gbc);
+        panel.add(get_message);
+
+        gbc.gridwidth = 300;
+        gbl.setConstraints(send_message, gbc);
+        panel.add(send_message);
+
+        gbc.gridheight = 50;
+        gbl.setConstraints(button, gbc);
+        panel.add(button);
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void run() {
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                stringBuffer = stringBuffer.append(send_message.getText());
+                send_message.setText("");
+            }
+        });
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_ENTER){
+                    stringBuffer = stringBuffer.append(send_message.getText());
+                    send_message.setText("");
+                }
+            }
+        });
+        get_message.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_ENTER){
+                    stringBuffer = stringBuffer.append(send_message.getText());
+                    send_message.setText("");
+                }
+            }
+        });
     }
 }
--- src/Net/NewServer.java
+++ src/Net/NewServer.java
@@ -1,14 +1,16 @@
 package Net;
 
-import javax.swing.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewServer {
     public static Map<Socket,Integer> sockets_id = Collections.synchronizedMap(new HashMap<>());
     public static int count = 0;
+    public static StringBuffer Serverinform = new StringBuffer();
     public static void main(String[] args) throws IOException{
         ServerSocket serverSocket = new ServerSocket(6666);
         System.out.println("Server start!");
@@ -17,6 +19,12 @@ public class NewServer {
             count++;
             NewServer.sockets_id.put(socket, count);
             System.out.println("no:"+sockets_id.get(socket)+"  Client in!");
+            StringBuilder temp = new StringBuilder();
+            for (Socket socket1 : NewServer.sockets_id.keySet()){
+                temp = temp.append(NewServer.sockets_id.get(socket1)+" ");
+            }
+            NewServer.Serverinform.replace(0, NewServer.Serverinform.length(), "@#*"+temp.toString()+"*#@");
+            System.out.println(NewServer.Serverinform);
             new Thread(new NewServersup(socket)).start();
         }
     }
@@ -33,9 +41,9 @@ class NewServersup implements Runnable {
 
     //读写
     @Override
-    public void run() {
+    public void run(){
+        SendAll();
         while ((temp = Read())!=null) {
-
             for (Socket s : NewServer.sockets_id.keySet()){
                 try {
                     OutputStream os = s.getOutputStream();
@@ -48,15 +56,23 @@ class NewServersup implements Runnable {
             }
 
         }
+        SendAll();
         Close();
     }
     //读
     private String Read(){
+
         try {
             return br.readLine();
         } catch (IOException e) {
             System.out.println("no "+NewServer.sockets_id.get(this.socket)+"  Client exit!");
             NewServer.sockets_id.remove(this.socket);
+            StringBuilder temp = new StringBuilder();
+            for (Socket socket1 : NewServer.sockets_id.keySet()){
+                temp = temp.append(NewServer.sockets_id.get(socket1)+" ");
+            }
+            NewServer.Serverinform.replace(0, NewServer.Serverinform.length(), "@#*"+temp.toString()+"*#@");
+            System.out.println(NewServer.Serverinform);
             return null;
         }
     }
@@ -67,6 +83,18 @@ class NewServersup implements Runnable {
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void SendAll(){
+        PrintStream printStream = null;
+        for (Socket socket : NewServer.sockets_id.keySet()){
+            try {
+                printStream = new PrintStream(socket.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            printStream.println(NewServer.Serverinform.toString());
         }
     }
 }
